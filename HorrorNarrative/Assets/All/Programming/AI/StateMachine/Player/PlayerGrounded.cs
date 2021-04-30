ffdbea6b34193c4848f60cc0cdd663951168745a
thuleanx <thuleanx@tutanota.com>
2021-04-30 16:09:33 -0400

    Acceleration Implemented

--- HorrorNarrative/Assets/All/Programming/AI/StateMachine/Player/PlayerGrounded.cs
+++ HorrorNarrative/Assets/All/Programming/AI/StateMachine/Player/PlayerGrounded.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using Thuleanx.Controls;
+using Thuleanx.Math;
 
 namespace Thuleanx.AI {
 	
 	[CreateAssetMenu(fileName = "PlayerGrounded", menuName = "~/StateMachine/Player/PlayerGrounded", order = 0)]
 	public class PlayerGrounded : PlayerState {
+		[SerializeField, Tooltip("Exponential acceleration. Higher means reaching target faster. ")]
+		float AccelerationLambda=8f;
+
 		public override void OnUpdate() {
 			base.OnUpdate();
 
 			if (PlayerAgent.CanControl) {
 				Vector2 idealMovement = InputController.Instance.Movement * PlayerAgent.Context.MoveVelocity;
-				PlayerAgent.LocalContext.Velocity = idealMovement;
+				PlayerAgent.LocalContext.Velocity = Calc.Damp(PlayerAgent.LocalContext.Velocity, idealMovement, AccelerationLambda, Time.deltaTime);
 			}
 		}
 	}
